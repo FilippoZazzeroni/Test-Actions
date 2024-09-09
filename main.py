eef2d6868577e43699e4b3cd0dc898de61b63b80
Filippo Zazzeroni <fzazzeroni@F7W3CYJHMV.fritz.box>
2024-09-09 10:30:05 +0200

    dumping to file

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import requests
+import json
 
-def fetch_latest_release_from_acorn() -> str|None:
+def fetch_latest_release_from_acorn() -> dict|None:
     owner = "FirefoxUX" 
     repo = "acorn-icons"    
     url = f"https://api.github.com/repos/{owner}/{repo}/releases/latest"
     response = requests.get(url=url)
     if response.status_code == 200:
         print(response.json())
-        return response.json()["id"]
+        return response.json()
     
-print(fetch_latest_release_from_acorn())
+
+latest_release_obj = fetch_latest_release_from_acorn()
+
+file = open("test.json", "w")
+json.dump(latest_release_obj, fp=file)
+file.close()
